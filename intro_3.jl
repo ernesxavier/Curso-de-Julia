1162ec9b517c79229ddc0596e81fc13b19b8871a
ernesxavier <ernesxavier@gmail.com>
2024-11-14 20:58:14 -0600

    add code in file intro

--- intro_3.jl
+++ intro_3.jl
@@ -4,9 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 2076e44b-a7fc-4da0-9f84-2a966085b413
-using PlutoUI, Random
-
 # ╔═╡ 057a2be0-a169-11ef-3717-df8e196b30cd
 md"""
 # Flujo de Control 
@@ -42,13 +39,15 @@ c
 # ╔═╡ a159ea9a-2e24-40b2-b70f-8fb0ad013d67
 d
 
+# ╔═╡ 2d7a344c-249e-4408-a81f-5da5cb668dd5
+if a > b 
+	println("$a es mayor que $b")
+end 
+
 # ╔═╡ a408e0da-c58b-4617-9035-5258b5acd300
 if a < b 
 println("$a es mayor que $b")
 end 
-
-# ╔═╡ 5a029d74-dc28-47df-a09b-4ce9f761d98c
-
 
 # ╔═╡ 160597f5-d61f-41f6-a662-ddab0d735b49
 if a > b 
@@ -67,13 +66,280 @@ println("$a es menor que $b")
 end 
 
 # ╔═╡ 67c837ac-7f59-4aaa-a8d2-d93e30b7a35a
+if a < b && b < c
+	println("$c es mayor que $b y $b es mayor que $a")
+else 
+	println("$a es menor que $b")	
+end
 
+# ╔═╡ 967c68c8-894e-4487-911d-e23ad28e1a26
+xx =  a < b && b < c
 
 # ╔═╡ 852f371a-24c6-4aae-be07-d22e547a2088
-
+if xx
+	println("$c es mayor que $b y $b es mayor que $a")
+else 
+	println("$a es menor que $b")	
+end
 
 # ╔═╡ 6a366959-077d-4de2-90f8-801731c73802
+if a > b 
+	a + b
+else c < d  
+	a - b
+end 
 
+# ╔═╡ de89b451-e7de-4034-b01b-dea08d59888c
+md"""
+### Forma abreviada
+
+`regla que evaluo como verdadera ? lo que quiero que haga si es verdadero : lo que hace si es falso`
+"""
+
+# ╔═╡ 6f4d794a-84c9-46d2-be7a-c3980fea2dba
+if a > b 
+println("$a es mayor que $b")
+else 
+println("$a es menor que $b")	
+end 
+
+# ╔═╡ bf37f65b-ee75-4f23-9107-67b5211efe00
+a > b ? println("$a es mayor que $b") : println("$a es menor que $b")
+
+# ╔═╡ 3d4aa103-e600-4a06-a7be-e9a5087dc857
+a > b ? a +b : a-b
+
+# ╔═╡ e36d28ac-88a9-4d93-9614-a24888e71f29
+a < b ? c * d : d * b
+
+# ╔═╡ 519c9f2d-e1ca-4fd2-8b84-39baa1f74084
+md"""
+## Bucles (loops)
+"""
+
+# ╔═╡ f5e7d6c2-adda-4c12-b85a-6dbf1c245794
+md"""
+### `while`
+"""
+
+# ╔═╡ 4f9d7e50-d3f9-4a95-a9d0-2ee6de283a49
+i = 1
+
+# ╔═╡ d7effc4b-592c-4622-952c-e994e0c683f8
+while i <= 10
+	println(i)
+	i += 1 #(i= i + 1)
+end
+
+# ╔═╡ 54ea9da4-f23c-40d7-b0df-b52879482d0a
+while i < 10
+	println(i)
+	i += 1 #(i= i + 1)
+end
+
+# ╔═╡ 0b97d957-3c4e-4c61-8a2a-f68f0d747ce4
+while i < 5
+	println(i)
+	i += 0.1 #(i(t+1)= i(t) + 1)
+end
+
+# ╔═╡ fd6a34f3-f859-4da5-be69-2be353df66db
+while i <= 5
+	println(i)
+	i += 0.1 #(i(t+1)= i(t) + 1)
+end
+
+# ╔═╡ 6ff9ffe0-71eb-4d17-a3bf-8dba90833b88
+while i <= 5000
+	println(i)
+	i += 1 #(i(t+1)= i(t) + 1)
+end
+
+# ╔═╡ 7679cb21-3abf-48a6-bbf7-36d1b0c5d6ae
+i
+
+# ╔═╡ 60af22da-6f69-428c-a06f-e5fb7c14913e
+while true 
+	println(i)
+
+	if i >=15 
+		break
+	end
+
+	i += 3
+end
+
+# ╔═╡ b5609886-dcd2-4a2d-bfc8-9a4c6243b046
+j = 1
+
+# ╔═╡ de266f14-fce3-4de7-a0b8-f2c3bc451d33
+begin
+while j < 5
+	println(j)
+	j += 0.1 #(i(t+1)= i(t) + 1)
+end
+end
+
+# ╔═╡ ca673c7a-ba46-4f6b-850a-b47af62c0483
+j
+
+# ╔═╡ 7d9e17f6-4a5c-4a4c-9382-571518433e8a
+while i <= 10
+	println(i)
+	i += 1 #(i= i + 1)
+end
+
+# ╔═╡ c3390ea9-b6fb-462b-97f2-a40128e65727
+md"""
+### `for`
+"""
+
+# ╔═╡ b2075cc2-def9-48b5-b5b3-d062eb80bc73
+for i in 1:10
+	println(i)
+end
+
+# ╔═╡ c72639b3-f529-4a30-bab9-66e7e762bf33
+k
+
+# ╔═╡ 3b6fea22-17ea-4038-866f-1e895daebfd3
+for k in 5:25
+	println(k)
+end
+
+# ╔═╡ f44013ce-78ab-490d-91e7-6c52895a624c
+for k in 5:5:25
+	println(k)
+end
+
+# ╔═╡ 06fbeeb6-5c80-4288-8cba-840bc5d36659
+for j in -10:10
+	println(j)
+end
+
+# ╔═╡ b7258910-7cb4-4776-9c07-6f221f25a7a4
+for h in 10:-1:1
+	println(h)
+end
+
+# ╔═╡ 4afa210f-2486-439e-a85c-e54b134433d7
+for caracteres in 'a':'z'
+	println(caracteres)
+end
+
+# ╔═╡ b1db367d-2cdd-4e54-8e1a-f0e8eff98c6c
+for caracteres in 'z':-1:'a'
+	println(caracteres)
+end
+
+# ╔═╡ 135dcad3-c174-445b-84cf-2d70243eee5d
+for caracteres in 'z':-5:'a'
+	println(caracteres)
+end
+
+# ╔═╡ bc95174d-efee-4cbe-93c1-098cf96b0d25
+for griego in 'α':'ω'
+	println(griego)
+end
+
+# ╔═╡ 2b05f4c0-831d-4120-8167-14a20d52b45b
+a
+
+# ╔═╡ 0c9aafab-1320-4d4c-8366-4193fbb0ce27
+x = 25
+
+# ╔═╡ 16426885-efe5-418c-b577-7a15bbecda42
+for i in a:x
+	println(i)
+end
+
+# ╔═╡ 7a042c9a-da62-4ae5-b46a-f42ec19ba1d3
+T = 200
+
+# ╔═╡ c82f84fe-b23e-4c4a-867f-8a65da18c2d0
+for i in 1:10:T
+	println(i)
+end
+
+# ╔═╡ 248119e0-b58f-4c0b-9709-3e5d4b08b9d9
+for i in 0:10:T
+	println(i)
+end
+
+# ╔═╡ 129310ca-4b22-4300-8202-8c40cd52d16a
+M = rand(10, 10)
+
+# ╔═╡ 5b88f164-a77f-4208-b699-7ea03e02255f
+CV = M[:]
+
+# ╔═╡ e2d74730-925f-46a1-b7e1-9eb4ef2d1193
+V = zeros(length(CV))
+
+# ╔═╡ c89f8e50-7003-44c1-8e05-16bf4285c10d
+CV[1]
+
+# ╔═╡ 3be8783e-d09e-4035-8f66-1e4ef8f550fc
+CV[2]
+
+# ╔═╡ f892039e-1e24-491e-8e64-2093a481cb80
+CV[end]
+
+# ╔═╡ f6a90c9e-fefd-4c42-a2d9-8ef80c144b08
+for i in 1:length(CV)
+	V[i] = CV[i] + 1
+end
+
+# ╔═╡ d9b861f4-2264-4b08-9cd2-e9c2db8592ed
+V
+
+# ╔═╡ 2338cef2-85bd-49c0-b71b-5bbf047a7d61
+V[1]
+
+# ╔═╡ ed95b586-ba39-4dba-8b9d-e5c7cbae320c
+CV[1]
+
+# ╔═╡ 961b007e-0e65-4c14-96a5-65fdfb541a9d
+V[begin]
+
+# ╔═╡ 998fafb7-c9e1-4752-9d4c-30260890eb3d
+CV[begin]
+
+# ╔═╡ 62608571-5718-4ac2-938c-daab6e7f74b2
+V[end]
+
+# ╔═╡ bda98bf2-d08e-431e-b446-3497b64a8312
+CV[end]
+
+# ╔═╡ 4af04eb1-83d4-47cf-82c5-8a9ec361129d
+CV .+ 1
+
+# ╔═╡ b076df1d-a65c-4f57-a84c-122a610554fc
+for i ∈ eachindex(CV)
+	V[i] = CV[i] + V[i]
+end
+
+# ╔═╡ a8aeefa5-ce04-4e55-893e-70c1e3af7864
+V
+
+# ╔═╡ 81b71348-5521-43c8-a65e-1d3a54ec70e9
+M_1 = []
+
+# ╔═╡ 2d061248-0b43-49f3-90d1-d6ddf1531630
+M_2 = zeros(15)
+
+# ╔═╡ cf1f5d07-c292-45ad-92fc-e0e26a285744
+for j in 1: 15
+	push!(M_1, j)
+end
+
+# ╔═╡ a96de136-28a8-479d-8bfe-cb905ed1a0ab
+M_1
+
+# ╔═╡ 2076e44b-a7fc-4da0-9f84-2a966085b413
+using PlutoUI, Random
+
+# ╔═╡ 6ccc7d21-2131-4257-a068-19fbdfb11883
+using Random
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -376,12 +642,71 @@ version = "17.4.0+2"
 # ╠═f065e082-6b74-4d41-870f-6e8f77f9bf6b
 # ╠═4346f444-4de9-43dc-a1ed-dffc8c7a36ed
 # ╠═a159ea9a-2e24-40b2-b70f-8fb0ad013d67
+# ╠═2d7a344c-249e-4408-a81f-5da5cb668dd5
 # ╠═a408e0da-c58b-4617-9035-5258b5acd300
-# ╠═5a029d74-dc28-47df-a09b-4ce9f761d98c
 # ╠═160597f5-d61f-41f6-a662-ddab0d735b49
 # ╠═8b901650-64c9-4653-a2b7-5f99e3a4361f
 # ╠═67c837ac-7f59-4aaa-a8d2-d93e30b7a35a
+# ╠═967c68c8-894e-4487-911d-e23ad28e1a26
 # ╠═852f371a-24c6-4aae-be07-d22e547a2088
 # ╠═6a366959-077d-4de2-90f8-801731c73802
+# ╠═de89b451-e7de-4034-b01b-dea08d59888c
+# ╠═6f4d794a-84c9-46d2-be7a-c3980fea2dba
+# ╠═bf37f65b-ee75-4f23-9107-67b5211efe00
+# ╠═3d4aa103-e600-4a06-a7be-e9a5087dc857
+# ╠═e36d28ac-88a9-4d93-9614-a24888e71f29
+# ╠═519c9f2d-e1ca-4fd2-8b84-39baa1f74084
+# ╠═f5e7d6c2-adda-4c12-b85a-6dbf1c245794
+# ╠═4f9d7e50-d3f9-4a95-a9d0-2ee6de283a49
+# ╠═d7effc4b-592c-4622-952c-e994e0c683f8
+# ╠═54ea9da4-f23c-40d7-b0df-b52879482d0a
+# ╠═0b97d957-3c4e-4c61-8a2a-f68f0d747ce4
+# ╠═fd6a34f3-f859-4da5-be69-2be353df66db
+# ╠═6ff9ffe0-71eb-4d17-a3bf-8dba90833b88
+# ╠═7679cb21-3abf-48a6-bbf7-36d1b0c5d6ae
+# ╠═60af22da-6f69-428c-a06f-e5fb7c14913e
+# ╠═b5609886-dcd2-4a2d-bfc8-9a4c6243b046
+# ╠═de266f14-fce3-4de7-a0b8-f2c3bc451d33
+# ╠═ca673c7a-ba46-4f6b-850a-b47af62c0483
+# ╠═7d9e17f6-4a5c-4a4c-9382-571518433e8a
+# ╠═c3390ea9-b6fb-462b-97f2-a40128e65727
+# ╠═b2075cc2-def9-48b5-b5b3-d062eb80bc73
+# ╠═c72639b3-f529-4a30-bab9-66e7e762bf33
+# ╠═3b6fea22-17ea-4038-866f-1e895daebfd3
+# ╠═f44013ce-78ab-490d-91e7-6c52895a624c
+# ╠═06fbeeb6-5c80-4288-8cba-840bc5d36659
+# ╠═b7258910-7cb4-4776-9c07-6f221f25a7a4
+# ╠═4afa210f-2486-439e-a85c-e54b134433d7
+# ╠═b1db367d-2cdd-4e54-8e1a-f0e8eff98c6c
+# ╠═135dcad3-c174-445b-84cf-2d70243eee5d
+# ╠═bc95174d-efee-4cbe-93c1-098cf96b0d25
+# ╠═2b05f4c0-831d-4120-8167-14a20d52b45b
+# ╠═0c9aafab-1320-4d4c-8366-4193fbb0ce27
+# ╠═16426885-efe5-418c-b577-7a15bbecda42
+# ╠═c82f84fe-b23e-4c4a-867f-8a65da18c2d0
+# ╠═7a042c9a-da62-4ae5-b46a-f42ec19ba1d3
+# ╠═248119e0-b58f-4c0b-9709-3e5d4b08b9d9
+# ╠═6ccc7d21-2131-4257-a068-19fbdfb11883
+# ╠═129310ca-4b22-4300-8202-8c40cd52d16a
+# ╠═5b88f164-a77f-4208-b699-7ea03e02255f
+# ╠═e2d74730-925f-46a1-b7e1-9eb4ef2d1193
+# ╠═c89f8e50-7003-44c1-8e05-16bf4285c10d
+# ╠═3be8783e-d09e-4035-8f66-1e4ef8f550fc
+# ╠═f892039e-1e24-491e-8e64-2093a481cb80
+# ╠═f6a90c9e-fefd-4c42-a2d9-8ef80c144b08
+# ╠═d9b861f4-2264-4b08-9cd2-e9c2db8592ed
+# ╠═2338cef2-85bd-49c0-b71b-5bbf047a7d61
+# ╠═ed95b586-ba39-4dba-8b9d-e5c7cbae320c
+# ╠═961b007e-0e65-4c14-96a5-65fdfb541a9d
+# ╠═998fafb7-c9e1-4752-9d4c-30260890eb3d
+# ╠═62608571-5718-4ac2-938c-daab6e7f74b2
+# ╠═bda98bf2-d08e-431e-b446-3497b64a8312
+# ╠═4af04eb1-83d4-47cf-82c5-8a9ec361129d
+# ╠═b076df1d-a65c-4f57-a84c-122a610554fc
+# ╠═a8aeefa5-ce04-4e55-893e-70c1e3af7864
+# ╠═81b71348-5521-43c8-a65e-1d3a54ec70e9
+# ╠═2d061248-0b43-49f3-90d1-d6ddf1531630
+# ╠═cf1f5d07-c292-45ad-92fc-e0e26a285744
+# ╠═a96de136-28a8-479d-8bfe-cb905ed1a0ab
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
