40bba054dc299956e9758b67967f77baf8b56a39
ernesxavier <ernesxavier@gmail.com>
2025-01-29 13:04:40 -0600

    Create intro_1-checkpoint.jl

---
+++ .ipynb_checkpoints/intro_1-checkpoint.jl
@@ -0,0 +1,65 @@
+### A Pluto.jl notebook ###
+# v0.20.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8640cbe6-9be5-11ef-0d44-01e82257f517
+md"""
+# Titulo 1
+"""
+
+# ╔═╡ 1ebfbfec-02a4-45f1-a2a7-e5c3ee55d5bb
+md"""
+## Titulo 2
+"""
+
+# ╔═╡ 63c998fd-84e3-4bc4-bac9-15f974da1781
+md"""
+### Titulo 3
+#### Titulo 4
+##### Titulo 5
+###### Titulo 6
+"""
+
+# ╔═╡ a00252a0-ad08-41b6-b303-04e3026c6305
+md"""
+Este es un parrafo de texto. 
+"""
+
+# ╔═╡ 12724bef-5389-4cbb-9d02-9256924ead0d
+md"""
+- Lista a
+- Lista b
+"""
+
+# ╔═╡ f2c789e6-2242-404d-93d6-c195b39fcd39
+md"""
+*Esto es cursiva*
+
+**Esto es negrita**
+"""
+
+# ╔═╡ e7ccfe6e-a1fd-4fd9-8fef-e4d0843e642b
+md"""
+$F(x) = x^2 - 10$
+"""
+
+# ╔═╡ 19a68137-e911-4edb-9855-418fb3afdfbd
+md"""
+Esta es una ecuación en el texto $g(x) = 3x^3$
+"""
+
+# ╔═╡ 2dc89611-08e5-4ab7-baf0-d192717fed06
+
+
+# ╔═╡ Cell order:
+# ╠═8640cbe6-9be5-11ef-0d44-01e82257f517
+# ╠═1ebfbfec-02a4-45f1-a2a7-e5c3ee55d5bb
+# ╠═63c998fd-84e3-4bc4-bac9-15f974da1781
+# ╠═a00252a0-ad08-41b6-b303-04e3026c6305
+# ╠═12724bef-5389-4cbb-9d02-9256924ead0d
+# ╠═f2c789e6-2242-404d-93d6-c195b39fcd39
+# ╠═e7ccfe6e-a1fd-4fd9-8fef-e4d0843e642b
+# ╠═19a68137-e911-4edb-9855-418fb3afdfbd
+# ╠═2dc89611-08e5-4ab7-baf0-d192717fed06
